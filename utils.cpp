3ddd9c0eef780c09f9af1147f6f1d942b6c6452c
kamimrcht <marchetcamille@gmail.com>
2020-02-14 23:13:21 +0100

    le cépépé

---
+++ utils.cpp
@@ -0,0 +1,21 @@
+#include "utils.h"
+//take an input file and create all the bucket files
+void create_buckets(const string& filename)
+{
+}
+
+string get_line_fasta(ifstream& in)
+{
+}
+
+vector<qgram> get_super_kmers(const string& line)
+{
+}
+
+minimizer get_minimizer(qgram)
+{
+}
+
+void super_kmer_to_buckets(const string& super_kmer_file)
+{
+}
